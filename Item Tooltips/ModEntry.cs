01e1e8f0e2b2556d7e7560243b417c6c163494a1
Jared <jaredlll08@gmail.com>
2020-12-28 06:18:57 +0200

    Ported to latest SDV

--- Item Tooltips/ModEntry.cs
+++ Item Tooltips/ModEntry.cs
@@ -1,37 +1,99 @@
+using System;
+using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 
-namespace StardewValley_SMAPI_CICD
+namespace Item_Tooltips
 {
     public class ModEntry : Mod
     {
-        /*********
-        ** Public methods
-        *********/
-        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
-        /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.Display.RenderingHud += DisplayOnRenderingHud;
         }
 
+        private int _yValue;
+        private int _fade;
+        private int _fadeOut;
+        private const int FadeMax = 90;
+        private Item _item;
 
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        private void DisplayOnRenderingHud(object sender, RenderingHudEventArgs e)
         {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
+            if (Game1.CurrentEvent != null)
+            {
                 return;
+            }
 
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+            if (Game1.activeClickableMenu != null)
+                return;
+            var center = Game1.player.GetBoundingBox().Center;
+            var globalPosition = new Vector2(center.X, center.Y);
+            var local = Game1.GlobalToLocal(Game1.viewport, globalPosition);
+            bool flag;
+            if (Game1.options.pinToolbarToggle)
+            {
+                flag = false;
+            }
+            else
+            {
+                flag = local.Y > (double) (Game1.viewport.Height / 2 + 64);
+            }
+
+            var num = Utility.makeSafeMarginY(8);
+            if (!flag)
+            {
+                _yValue = Game1.viewport.Height;
+                _yValue += 8;
+                _yValue -= num;
+                _yValue -= 16;
+                _yValue -= 24;
+                _yValue -= 8;
+            }
+            else
+            {
+                _yValue = 112;
+                _yValue -= 8;
+                _yValue += num;
+                _yValue += 96 + 8;
+            }
+
+            if (_item == null || _item != Game1.player.CurrentItem)
+            {
+                _item = Game1.player.CurrentItem;
+                _fade = FadeMax;
+                _fadeOut = FadeMax/3;
+            }
+
+            if (_fade <= 0 && _fadeOut <= 0 || _item == null) return;
+            _fade--;
+
+            if (_fade <= 0)
+            {
+                _fadeOut--;
+            }
+
+            var currentItemDisplayName = Game1.player.CurrentItem.DisplayName;
+            var x = Game1.viewport.Width / 2 - Game1.dialogueFont.MeasureString(currentItemDisplayName).Length() / 2;
+
+            var black = Color.Black;
+            var white = Color.White;
+
+
+            var cutLength = (int)Remap(_fadeOut, FadeMax/3, 0, currentItemDisplayName.Length, 0);
+
+
+            Game1.spriteBatch.DrawString(Game1.dialogueFont, currentItemDisplayName.Substring(0,cutLength), new Vector2(x - 4, _yValue - 96 - 8), black);
+            Game1.spriteBatch.DrawString(Game1.dialogueFont, currentItemDisplayName.Substring(0,cutLength), new Vector2(x + 4, _yValue - 96 - 8), black);
+            Game1.spriteBatch.DrawString(Game1.dialogueFont, currentItemDisplayName.Substring(0,cutLength), new Vector2(x, _yValue - 96 - 4), black);
+            Game1.spriteBatch.DrawString(Game1.dialogueFont, currentItemDisplayName.Substring(0,cutLength), new Vector2(x, _yValue - 96 - 12), black);
+
+            Game1.spriteBatch.DrawString(Game1.dialogueFont, currentItemDisplayName.Substring(0,cutLength), new Vector2(x, _yValue - 96 - 8), white);
+        }
+
+        public static float Remap (float value, float from1, float to1, float from2, float to2) {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
         }
     }
 }
-
